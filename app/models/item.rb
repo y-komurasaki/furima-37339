64f2858b9cd9f9998530fd6e37a9b9f7c3a09d91
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-04-17 10:34:32 +0900

    item.rbにてバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  with_options presence: true do
+    validates :user_id
+    validates :name
+    validates :info
+    validates :category_id
+    validates :sipping_cost_id
+    validates :prefecture_id
+    validates :sipping_date_id
+    validates :price
 end
