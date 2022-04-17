cc99b0660a5b8d7240c18b23f2dda6ace2204d47
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-04-17 14:25:32 +0900

    商品投稿機能におけるActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :user
 
   has_one_attached :image
+  
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :sipping_cost
+  belongs_to :prefecture_id
+  belongs_to :sipping_date_id
+
 
   with_options presence: true do
     validates :user_id
@@ -12,6 +21,14 @@ class Item < ApplicationRecord
     validates :prefecture_id
     validates :sipping_date_id
     validates :price
+  end  
+
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :user_id
+    validates :category_id
+    validates :sipping_cost_id
+    validates :prefecture_id
+    validates :sipping_date_id
   end  
 
 end
