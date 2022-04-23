6d6e461a73bebe0bbd4e88484b740afb193aa6d9
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-04-23 15:38:48 +0900

    ユーザーが紐付いていなければ投稿できないテストコード追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -88,6 +88,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than 9999999")
       end
+
+      it 'ユーザーが紐付いていなければ投稿できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
   
   
     end
