7a8f17b8a57c1afc85409b8537cb075a64fc6cd9
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-04-23 14:26:36 +0900

    バリデーションの手直し

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,19 +13,23 @@ class Item < ApplicationRecord
 
 
   with_options presence: true do
-    validates :user_id
+    validates :image
     validates :name
     validates :info
     validates :category_id
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price
   end  
 
+  with_options format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
+  end
+
   with_options numericality: { other_than: 0 , message: "can't be blank"} do
-    validates :user_id
     validates :category_id
+    validates :item_status_id
     validates :shipping_cost_id
     validates :prefecture_id
     validates :shipping_date_id
