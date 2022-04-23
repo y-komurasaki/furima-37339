7541e97969a242d0b309bdcfa61bddb3f358db65
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-04-23 15:37:42 +0900

    一部修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,7 +3,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @items = Item.includes(:user).order('created_at DESC')
   end
 
   def new
