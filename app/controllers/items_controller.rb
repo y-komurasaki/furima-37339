ad8ae0825ce6d64ddf0e70829645f54f0b3b0606
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-04-24 11:13:08 +0900

    itemsコントローラーにshowアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,13 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index,:show]
 
   def index
     @items = Item.all.order(created_at: :desc)
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   def new
