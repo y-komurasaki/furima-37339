2cafb8c1c6fe55e7a84ed03603e59483bb5bb301
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-04-25 20:08:57 +0900

    一部修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,8 +40,8 @@ class ItemsController < ApplicationController
   def destroy
     if @item.user_id == current_user.id
       @item.destroy
+    end
       redirect_to root_path
-    end  
   end
 
   
