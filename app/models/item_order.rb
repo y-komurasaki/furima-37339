7be2ad205975dea73b2890a6fc740f54fb58806e
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-05-01 14:20:00 +0900

    一部修正

--- app/models/item_order.rb
+++ app/models/item_order.rb
@@ -5,16 +5,23 @@ class ItemOrder
 
 
   with_options presence: true do
-    validates :prefecture_id, :city, :block, :phone_number, :post_code
+    validates :city, :block, :phone_number, :post_code
   end
+
+  with_options numericality: { other_than: 0 , message: "can't be blank"} do  
+    validates :prefecture_id 
+  end  
 
    validates :phone_number, format: { with: /\A\d{11}\z/ }
    validates :post_code, length: { is: 8 } 
    validates :post_code, format: { with: /\A\d{3}[-]\d{4}\z/ } 
-    
+
 
   def save
-    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building_name, phone_number: phone_number)
-    Order.create(user_id: user_id, item_id: item_id)
+    
+    item_order = Order.create(user_id: user_id, item_id: item_id)
+
+    Address.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number)
+
   end
 end
