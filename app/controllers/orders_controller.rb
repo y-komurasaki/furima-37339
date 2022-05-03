9912b850f5243380659104c8edc3e969c1944c1c
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-05-04 07:33:28 +0900

    コントローラーにtokenを設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
   def create
     @item_order = ItemOrder.new(order_params)
     if @item_order.valid?
+      pay_item
       @item_order.save
       return redirect_to root_path
     else
@@ -23,7 +24,16 @@ class OrdersController < ApplicationController
   end
 
   def order_params
-    params.require(:item_order).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id )
+    params.require(:item_order).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number, :order_id).merge(token: params[:token], item_id: params[:item_id], user_id: current_user.id )
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_f96c4d6ba0b7a5020511fc22"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
