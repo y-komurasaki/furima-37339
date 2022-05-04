c5c78ec8d241900842fa698ee628f83167d7ec14
y-komurasaki <yuusuke3tamaria19880415@gmail.com>
2022-05-04 22:52:39 +0900

    indexアクション一部修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,11 @@ class OrdersController < ApplicationController
   before_action :set_item, only: [:index, :create]
 
   def index
-    @item_order = ItemOrder.new
+    if user_signed_in? && current_user.id != @item.user_id && @item.order == nil
+     @item_order = ItemOrder.new
+    else
+      redirect_to root_path
+    end 
   end
 
   def create
